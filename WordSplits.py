5d4379965276c9374b7ae14e4d9047863c03935a
Alexander Diaz <alexgdiaz98@gmail.com>
2019-03-05 00:10:41 -0500

    Working Save

--- WordSplits.py
+++ WordSplits.py
@@ -1,93 +1,114 @@
 import re
-with open('WordSplits.txt', 'r') as doc:
-    with open('Enable1.txt', 'r') as doc2:
-        counts = [0,0,0,0,0,0,0,0,0,0]
-        patterns = doc.readlines()
-        words = doc2.readlines()
-        strip = ".0123456789 \n "
-        counter = 0
-        for word in words:
-            word = word.replace("\n", "")
-            #print("Word = " + word)
-            patternTable = []
-            hyphenTable = []
-            for i in range(len(word)-1):
-                hyphenTable.append(0)
-            for pattern in patterns:
-                pattern = pattern.replace("\n", "")
-                alpha = pattern
-                for char in strip:
-                    alpha = alpha.replace(char,"")
-                if pattern.startswith("."):
-                    if word.startswith(alpha):
-                        #print(word + " matched with " + pattern + " at the start")
-                        patternTable.append([pattern, alpha]) # row in detailed example table
-                        counter += 1
-                elif pattern.endswith("."):
-                    if word.endswith(alpha):
-                        #print(word + " matched with " + pattern + " at the end")
-                        patternTable.append([pattern, alpha])
-                        counter += 1
-                else:
-                    if alpha in word:
-                        #print(word + " matched with " + pattern + " in the middle")
-                        patternTable.append([pattern, alpha])
-                        counter += 1
-            '''
-            print(word + ": ", end=" ")
-            for pair in patternTable:
-                print(pair[0], end=" ")
-            print()
-            '''
-            for pair in patternTable:
-                pair[0] = pair[0].replace('.', "")
-                indeces = [m.start() for m in re.finditer(pair[1], word)]
-                #print(", ".join(str(x) for x in indeces))
-                comp = 0
-                for ind in range(len(pair[0])): #for each character in the pattern
-                    if pair[0][ind] in "123456789":
-    
-                        #print("ind = " + str(ind))
-                        #print("hyphenTable length = " + str(len(hyphenTable)))
-                        for i in indeces:
-                            if not (i + ind == 0 or i + ind - comp >= len(word)):
-                                #print("i = " + str(i) + " pair[0][ind] = " + pair[0][ind])
-                                if hyphenTable[i + ind - 1 - comp] < int(pair[0][ind]):
-                                    hyphenTable[i + ind - 1 - comp] = int(pair[0][ind])
-                        comp += 1
-            hyphenCount = 0
-            for i in range(len(hyphenTable)):
-                if hyphenTable[i] % 2 == 1:
-                    word = word[:i+hyphenCount+1] + '-' + word[i+hyphenCount+1:]
-                    hyphenCount += 1
-            '''
-            print(word + " " + ", ".join(str(x) for x in hyphenTable))
-            print()
-            '''
-            counts[hyphenCount] += 1
-            if counter % 1000 == 0:
-                print(str(counter))
-            #print(word + "\t\t\t" + str(hyphenCount) + " total is now " + str(counts[hyphenCount]))
-            
-            
-        for i in range(len(counts)):
-            print(str(i) + " hyphens: " + str(counts[i]))
-'''
-Detailed Example:
+import sys
 
-m i s t r a n s l a t e
-| | | | | |2| | | | | | a2n
-| | |1| | | | | | | | | .mis1
-|2| | | | | | | | | | | m2is
-| | | | | |2|1|2| | | | 2n1s2
-| | | | | | |2| | | | | n2sl
-| | | | | | | |1|2| | | s1l2
-| | | | | | | |3| | | | s3lat
-| | | |4| | | | | | | | st4r
-| | | | | | | | | |4| | 4te.
-| | |1| | | | | | | | | 1tra
-m2i s1t4r a2n2s3l2a4t e
-| | | | | | | | | | | |
-m i s-t r a n s-l a t e
+if (len(sys.argv) == 2):
+	text_file = sys.argv[1]
+else:
+	text_file = "Enable1.txt"
 
-'''
+with open('WordSplits.txt', 'r') as pattern_doc:
+	with open(text_file, 'r') as word_doc:
+		counts = [0,0,0,0,0,0,0,0,0,0]
+		patterns = pattern_doc.readlines()
+		words = word_doc.readlines()
+		strip = ".0123456789 \n " # Characters removed from a pattern to make alphabetical string
+		counter = 0 # Number of patterns for a given word
+		for word in words:
+			word = word.replace("\n", "")
+			#print("Word = " + word)
+			patternTable = [] # Array of patterns found for given word -> patternTable[i] = [pattern, alpha]
+			hyphenTable = [] # Array of highest number found for each space between characters in word
+			for i in range(len(word)-1):
+				hyphenTable.append(0)
+			for pattern in patterns:
+				pattern = pattern.replace("\n", "")
+				alpha = pattern # Pattern = '.eq5ui5t' -> Alpha = 'equit'
+				for char in strip:
+					alpha = alpha.replace(char,"")
+				if pattern.startswith("."):
+					if word.startswith(alpha):
+						#print(word + " matched with " + pattern + " at the start")
+						patternTable.append([pattern, alpha]) # row in detailed example table
+						counter += 1
+				elif pattern.endswith("."):
+					if word.endswith(alpha):
+						#print(word + " matched with " + pattern + " at the end")
+						patternTable.append([pattern, alpha])
+						counter += 1
+				else:
+					if alpha in word:
+						#print(word + " matched with " + pattern + " in the middle")
+						patternTable.append([pattern, alpha])
+						counter += 1
+			'''
+			print(word + ": ", end=" ")
+			for pair in patternTable:
+				print(pair[0], end=" ")
+			print()
+			'''
+			for pair in patternTable:
+				#print("considering", pair[0])
+				if (pair[0][0] == '.'): # If pattern begins with '.'
+					pair[0] = pair[0].replace('.', "")
+					char_index = 0
+					for i in range(len(pair[0])):
+						if (pair[0][i] in "123456789"):
+							if (i == 0):
+								#print("Error: Hyphen Number occured before word begins")
+								pass
+							elif (char_index == len(word)):
+								#print("Hyphen Number at end of '.*' word", word, pair[0])
+								pass
+							elif (int(pair[0][i]) > hyphenTable[char_index-1]):
+								hyphenTable[char_index-1] = int(pair[0][i])
+						else:
+							char_index += 1
+				elif (pair[0][-1] == '.'): # If pattern ends with '.'
+					pair[0] = pair[0].replace('.', "")
+					char_index = 0
+					for i in range(len(pair[0])):
+						if (pair[0][i] in "123456789"):
+							if (i == len(pair[0])-1):
+								#print("Error: Hyphen Number occured after word ends")
+								pass
+							elif (len(pair[1]) == len(word) and char_index == 0 and i == 0):
+								#print("Hyphen Number at beginning of '*.' word", word, pair[0])
+								pass
+							elif (int(pair[0][i]) > hyphenTable[len(hyphenTable)-len(pair[1])+char_index]):
+								hyphenTable[len(hyphenTable)-len(pair[1])+char_index] = int(pair[0][i])
+						else:
+							char_index += 1
+				else: # If pattern has no '.'
+					indeces = [m.start() for m in re.finditer(pair[1], word)]
+					#print(pair[0], "found at indeces: ", end="")
+					#print(", ".join(str(x) for x in indeces))
+					for ind in indeces:
+						char_index = 0
+						for i in range(len(pair[0])): # For each character in the pattern
+							if (pair[0][i] in "123456789"):
+								if ((i != 0 or ind != 0) and (i != len(pair[0])-1 or ind != len(hyphenTable)-len(pair[1])+1)):
+									if (int(pair[0][i]) > hyphenTable[ind+char_index-1]):
+										hyphenTable[ind+char_index-1] = int(pair[0][i])
+								#else:
+								#	print("Detected Hyphen Number at the beginning or end of a word", pair[0], word)
+							else:
+								char_index += 1
+			hyphenCount = 0
+			for i in range(len(hyphenTable)):
+				if hyphenTable[i] % 2 == 1:
+					word = word[:i+hyphenCount+1] + '-' + word[i+hyphenCount+1:]
+					hyphenCount += 1
+			'''
+			print(word + " " + ", ".join(str(x) for x in hyphenTable))
+			print(
+			'''
+			counts[hyphenCount] += 1
+			
+			if counter % 1000 == 0:
+				print(str(counter),"word:", word)
+			
+			#print(word, "has", str(hyphenCount), "hyphens. Hyphen Table:", str(hyphenTable), '\n')
+			
+			
+		for i in range(len(counts)):
+			print("Number of words with", str(i), "hyphens:", str(counts[i]))
